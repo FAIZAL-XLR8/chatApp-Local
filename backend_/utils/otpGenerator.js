c0d99a5009a3275aea812716d1291a16559dea83
FAIZAL-XLR8 <slif858@gmail.com>
2026-01-15 19:11:35 +0530

    frontend

--- backend_/utils/otpGenerator.js
+++ backend_/utils/otpGenerator.js
@@ -1,5 +1,5 @@
 const otpGeneraete = () => {
-    return Math.floor(1000000 + Math.random() * 9000).toString();
-    //7 digits otp
+    return Math.floor(100000 + Math.random() * 9000).toString();
+    // 6 digits otp
 }
 module.exports = otpGeneraete;
